175e69f1724b3b1f5acca01a500c25f5454f3807
Wojciech Kania <wojtek@kania.sh>
2026-01-19 12:06:43 +0100

    add tanstak query and tanstack router eslint plugins

--- libraries/eslint/react-vite.cjs
+++ libraries/eslint/react-vite.cjs
@@ -1,6 +1,8 @@
 const globals = require("globals");
 const reactHooks = require("eslint-plugin-react-hooks");
 const reactRefresh = require("eslint-plugin-react-refresh");
+const pluginQuery = require("@tanstack/eslint-plugin-query");
+const pluginRouter = require("@tanstack/eslint-plugin-router");
 
 module.exports = [
   {
@@ -9,10 +11,14 @@ module.exports = [
       globals: globals.browser,
     },
     plugins: {
+      "@tanstack/router": pluginRouter,
+      "@tanstack/query": pluginQuery,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...(pluginRouter.configs.recommended?.rules ?? {}),
+      ...(pluginQuery.configs.recommended?.rules ?? {}),
       ...(reactHooks.configs.recommended?.rules ?? {}),
       ...(reactRefresh.configs.vite?.rules ?? {}),
     },
