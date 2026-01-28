75990d07c1b1150226d7e1cb693a374a8b9c3f1f
Wojciech Kania <wojtek@kania.sh>
2026-01-28 15:25:17 +0100

    show modal and confirm

--- apps/admin/src/routes/_auth/users/index.tsx
+++ apps/admin/src/routes/_auth/users/index.tsx
@@ -1,14 +1,45 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
+import Button from "@/components/form/Button";
 import AuthLayout from "@/components/layout/AuthLayout";
+import { useConfirm } from "@/components/overlays/confirm";
+import Modal from "@/components/overlays/Modal";
 
 export const Route = createFileRoute("/_auth/users/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const confirm = useConfirm();
   return (
-    <AuthLayout title="Users">
+    <AuthLayout
+      title="Users"
+      actions={
+        <div className="flex shrink-0 gap-2">
+          <Button
+            onClick={() => {
+              confirm("Czy na pewno chcesz aby wyskoczył modal?", () =>
+                { setIsModalOpen(true); },
+              );
+            }}
+          >
+            Confirm
+          </Button>
+          <Button
+            onClick={() => {
+              setIsModalOpen(!isModalOpen);
+            }}
+          >
+            Modal
+          </Button>
+        </div>
+      }
+    >
+      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} title="Modal">
+        <div>Hello "/_auth/users/"!</div>
+      </Modal>
       <div>Hello "/_auth/users/"!</div>
     </AuthLayout>
   );
