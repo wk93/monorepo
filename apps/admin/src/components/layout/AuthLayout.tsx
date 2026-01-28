4172a9281bcb6bbf5881a567cbb5090eb633d84f
Wojciech Kania <wojtek@kania.sh>
2026-01-28 15:12:45 +0100

    use tanstack links in auth layout

--- apps/admin/src/components/layout/AuthLayout.tsx
+++ apps/admin/src/components/layout/AuthLayout.tsx
@@ -8,6 +8,7 @@ import {
   MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link, linkOptions } from "@tanstack/react-router";
 import clsx from "clsx";
 import type { PropsWithChildren } from "react";
 
@@ -29,24 +30,21 @@ interface Props extends PropsWithChildren {
 
 const AuthLayout: React.FC<Props> = ({ title, children, actions }) => {
   const setToken = useAuthStore((s) => s.setToken);
-  const navigation = [
-    { name: "Dashboard", href: "#", current: true },
-    { name: "Team", href: "#", current: false },
-    { name: "Projects", href: "#", current: false },
-    { name: "Calendar", href: "#", current: false },
-    { name: "Reports", href: "#", current: false },
-  ];
-  const userNavigation = [
-    { name: "Your profile", href: "#" },
-    { name: "Settings", href: "#" },
+  const navigation = linkOptions([
+    { name: "Dashboard", to: "/" },
+    { name: "Team", to: "/users" },
+  ]);
+
+  const userNavigation = linkOptions([
+    { name: "Settings", to: "/", onClick: undefined },
     {
       name: "Sign out",
-      href: "#",
+      to: "/login",
       onClick: () => {
         setToken(null);
       },
     },
-  ];
+  ]);
 
   return (
     <div className="h-screen flex flex-col">
@@ -62,19 +60,15 @@ const AuthLayout: React.FC<Props> = ({ title, children, actions }) => {
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
                       {navigation.map((item) => (
-                        <a
+                        <Link
                           key={item.name}
-                          href={item.href}
-                          aria-current={item.current ? "page" : undefined}
+                          {...item}
                           className={clsx(
-                            item.current
-                              ? "bg-primary-950 text-white"
-                              : "text-white hover:bg-white/5",
-                            "rounded-md px-3 py-2 text-sm font-medium",
+                            "[.active]:bg-primary-950 [.active]:hover:bg-primary-950 text-white hover:bg-white/5 rounded-md px-3 py-2 text-sm font-medium",
                           )}
                         >
                           {item.name}
-                        </a>
+                        </Link>
                       ))}
                     </div>
                   </div>
@@ -108,13 +102,13 @@ const AuthLayout: React.FC<Props> = ({ title, children, actions }) => {
                       >
                         {userNavigation.map((item) => (
                           <MenuItem key={item.name}>
-                            <a
-                              href={item.href}
+                            <Link
+                              {...item}
                               className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                               onClick={item.onClick}
                             >
                               {item.name}
-                            </a>
+                            </Link>
                           </MenuItem>
                         ))}
                       </MenuItems>
@@ -141,63 +135,68 @@ const AuthLayout: React.FC<Props> = ({ title, children, actions }) => {
           </div>
 
           <DisclosurePanel className="border-b border-white/10 md:hidden">
-            <div className="space-y-1 px-2 py-3 sm:px-3">
-              {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  aria-current={item.current ? "page" : undefined}
-                  className={clsx(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-white hover:bg-white/5",
-                    "block rounded-md px-3 py-2 text-base font-medium",
-                  )}
-                >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-            <div className="border-t border-white/10 pt-4 pb-3">
-              <div className="flex items-center px-5">
-                <div className="shrink-0">
-                  <img
-                    alt=""
-                    src={user.imageUrl}
-                    className="size-10 rounded-full outline -outline-offset-1 outline-white/10"
-                  />
+            {({ close }) => (
+              <>
+                <div className="space-y-1 px-2 py-3 sm:px-3">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      {...item}
+                      onClick={() => {
+                        close();
+                      }}
+                      className={clsx(
+                        "[.active]:bg-primary-900  [.active]:hover:bg-primary-900 text-white block rounded-md px-3 py-2 text-base font-medium",
+                      )}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
                 </div>
-                <div className="ml-3">
-                  <div className="text-base/5 font-medium text-white">
-                    {user.name}
+                <div className="border-t border-white/10 pt-4 pb-3">
+                  <div className="flex items-center px-5">
+                    <div className="shrink-0">
+                      <img
+                        alt=""
+                        src={user.imageUrl}
+                        className="size-10 rounded-full outline -outline-offset-1 outline-white/10"
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base/5 font-medium text-white">
+                        {user.name}
+                      </div>
+                      <div className="text-sm font-medium text-gray-400">
+                        {user.email}
+                      </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-primary-500"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon aria-hidden="true" className="size-6" />
+                    </button>
                   </div>
-                  <div className="text-sm font-medium text-gray-400">
-                    {user.email}
+                  <div className="mt-3 space-y-1 px-2">
+                    {userNavigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        {...item}
+                        onClick={() => {
+                          item.onClick?.();
+                          close();
+                        }}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white"
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
                   </div>
                 </div>
-                <button
-                  type="button"
-                  className="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-primary-500"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon aria-hidden="true" className="size-6" />
-                </button>
-              </div>
-              <div className="mt-3 space-y-1 px-2">
-                {userNavigation.map((item) => (
-                  <DisclosureButton
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white"
-                  >
-                    {item.name}
-                  </DisclosureButton>
-                ))}
-              </div>
-            </div>
+              </>
+            )}
           </DisclosurePanel>
         </Disclosure>
         <header className="py-10">
