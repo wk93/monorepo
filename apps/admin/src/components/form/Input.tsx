35cfdca321a05f97fdeb8914ab6f2563c8fb7967
Wojciech Kania <wojtek@kania.sh>
2026-01-29 10:36:18 +0100

    upgrade input component

--- apps/admin/src/components/form/Input.tsx
+++ apps/admin/src/components/form/Input.tsx
@@ -1,12 +1,18 @@
-type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, "className">;
+import { Input as HeadlessInput, type InputProps } from "@headlessui/react";
+import React from "react";
 
-const Input: React.FC<Props> = ({ ...props }) => {
+type Props = Omit<InputProps, "className">;
+
+const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
-    <input
+    <HeadlessInput
+      ref={ref}
       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary-600 sm:text-sm/6"
       {...props}
     />
   );
-};
+});
+
+Input.displayName = "Input";
 
 export default Input;
